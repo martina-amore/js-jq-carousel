7b743891086dab075bf4f189c204ee975149f958
Martina Amore <martina.amore02@gmail.com>
2020-11-03 19:48:46 +0100

    Completo + bonus

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
-$(".next").click(function(){
-    var nextPhoto = $("img.active");
+function prevPhoto(){
+    var currentPhoto = $("img.active");
+    var currentCircle = $("i.active");
 
-    nextPhoto.removeClass("active");
-    nextPhoto.next().addClass("active");
+    currentPhoto.removeClass("active");
+    currentCircle.removeClass("active");
 
-})
+    if (currentPhoto.hasClass("first")) {
+        $("img.last").addClass("active");
+        $("i.last").addClass("active");
+
+    } else {
+        currentPhoto.prev().addClass("active");
+        currentCircle.prev().addClass("active");
+    }
+}
+
+function nextPhoto(){
+    var currentPhoto = $("img.active");
+    var currentCircle = $("i.active")
+
+    currentPhoto.removeClass("active");
+    currentCircle.removeClass("active");
+
+    if (currentPhoto.hasClass("last")) {
+        $("img.first").addClass("active");
+        $("i.first").addClass("active");
+
+    } else {
+        currentPhoto.next().addClass("active");
+        currentCircle.next().addClass("active");
+    }
+}
 
 $(".prev").click(function(){
-    var prevPhoto = $("img.active");
+    prevPhoto();
+})
 
-    prevPhoto.removeClass("active");
-    prevPhoto.prev().addClass("active");
+$(".next").click(function(){
+    nextPhoto();
+})
 
+$(document).keydown(function(){
+    if (event.which == 37){
+        prevPhoto();
+    }
+    else if (event.which == 39){
+        nextPhoto();
+    }
 })
